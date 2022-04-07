693d54a5a703fb829607adbae97905ad7e953471
Philipp Flotho <PhilippFlotho@gmail.com>
2022-04-08 01:19:10 +0200

    small changes

--- mask_the_face.py
+++ mask_the_face.py
@@ -9,14 +9,13 @@ from aux_functions import *
 
 class FaceMaskAugmentor:
     def __init__(self, mask_type="random", pattern="", pattern_weight=0.5, color="#0473e2", color_weight=0.5, code=""):
-        self.args = argparse.Namespace(
-            mask_type=mask_type,
-            pattern=pattern,
-            pattern_weight=pattern_weight,
-            color=color,
-            color_weight=color_weight,
-            code=code
-    )
+        self.params = {
+            mask_type:mask_type,
+            pattern:pattern,
+            pattern_weight:pattern_weight,
+            color:color,
+            color_weight:color_weight,
+            code:code}
 
     def __call__(self, img):
-        return mask_image(img, self.args)
+        return mask_image(img, self.params)
