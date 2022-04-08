3e67e677c6d4f7434f169ed66a05e921e9ad90b7
Doodle <saurabh.pandey.2171993@gmail.com>
2022-04-08 18:18:16 +0200

    color param issue on colab check

--- mask_the_face.py
+++ mask_the_face.py
@@ -8,7 +8,7 @@ from aux_functions import *
 
 
 class FaceMaskAugmentor:
-    def __init__(self, mask_type="random", pattern="", pattern_weight=0.5, color="#0473e2", color_weight=0.5, code="", verbose=False, pred_path="dlib_models/shape_predictor_68_face_landmarks.dat"):
+    def __init__(self, mask_type="random", pattern="", pattern_weight=0.5, color="", color_weight=0.5, code="", verbose=False, pred_path="dlib_models/shape_predictor_68_face_landmarks.dat"):
         download_dlib_model()
         self.args = argparse.Namespace(
             mask_type=mask_type,
