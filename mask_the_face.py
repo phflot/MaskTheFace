76a3afb89d24a7bff23341137cf0603225eeeef0
Doodle <saurabh.pandey.2171993@gmail.com>
2022-04-08 18:51:25 +0200

    issue still persist, reverting changes

--- mask_the_face.py
+++ mask_the_face.py
@@ -5,7 +5,6 @@
 import argparse
 import dlib
 from aux_functions import *
-import os
 
 class FaceMaskAugmentor:
     def __init__(self, mask_type="random", pattern="", pattern_weight=0.5, color="", color_weight=0.5, code="", verbose=False, pred_path="dlib_models/shape_predictor_68_face_landmarks.dat"):
@@ -21,7 +20,5 @@ class FaceMaskAugmentor:
             predictor=dlib.shape_predictor(pred_path),
             detector=dlib.get_frontal_face_detector()
          )
-        base_path = os.path.dirname(os.path.abspath(__file__))
-        print(base_path)
     def __call__(self, img):
         return mask_image(img, self.args)
