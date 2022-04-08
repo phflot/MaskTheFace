6905f4f9b8303bfcf277499e6faccade84269b69
Doodle <saurabh.pandey.2171993@gmail.com>
2022-04-08 04:38:31 +0200

    missing args values added i.e args.predictor and args.detector

--- mask_the_face.py
+++ mask_the_face.py
@@ -8,15 +8,18 @@ from aux_functions import *
 
 
 class FaceMaskAugmentor:
-    def __init__(self, mask_type="random", pattern="", pattern_weight=0.5, color="#0473e2", color_weight=0.5, code=""):
+    def __init__(self, mask_type="random", pattern="", pattern_weight=0.5, color="#0473e2", color_weight=0.5, code="", verbose=False, pred_path="dlib_models/shape_predictor_68_face_landmarks.dat"):
+        download_dlib_model()
         self.args = argparse.Namespace(
             mask_type=mask_type,
             pattern=pattern,
             pattern_weight=pattern_weight,
+            verbose=verbose,
             color=color,
             color_weight=color_weight,
-            code=code
-            )
-
+            code=code,
+            predictor=dlib.shape_predictor(pred_path),
+            detector=dlib.get_frontal_face_detector()
+         )
     def __call__(self, img):
         return mask_image(img, self.args)
